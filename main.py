c7820e40f00c0cfcc200c6831896eae6a648dbee
Ian Taylor <ian.m.taylor1@gmail.com>
2019-01-08 14:43:00 -0700

    Lots of improvements to script - allows for more automatic handling of errors.

--- main.py
+++ main.py
@@ -110,7 +110,6 @@ def load_games(espndf, ncaadf, session):
     return session.query(model.TempESPNGame), session.query(model.TempNCAAGame)
 
 
-
 # Get the games
 print()
 espndf, ncaadf = get_games(season, week, cachedir=cachedir, echo=True)
@@ -124,6 +123,13 @@ session = model.Session(bind=dbcon)
 # Load the games into the db
 espngames, ncaagames = load_games(espndf, ncaadf, session)
 
+# How many games are already in the database?
+print("Games already existing for these dates:",
+        session.query(model.Game).filter(model.Game.date.in_({g.date for g in ncaagames})).count(),
+        "games.")
+
+################################################################################
+
 # Check for games with unknown team names
 def find_unknown_teams(games):
     unknown = set()
@@ -135,22 +141,53 @@ def find_unknown_teams(games):
     return list(unknown)
 
 print()
+# Display unknown ESPN Teams
 print("Unknown ESPN Teams: ", end="")
 unknown_espn_teams = find_unknown_teams(espngames)
 if len(unknown_espn_teams) > 0:
     print(", ".join(unknown_espn_teams))
 else:
     print("None.")
+# Loop through them and prompt for teamids
+for t in unknown_espn_teams:
+    team = None
+    while team is None:
+        id = input("Enter team id for " + t + ": ")
+        team = session.query(model.Team).filter(model.Team.id == id).one_or_none()
+        print(team)
+        if input('Correct? (y/n) ').lower() != 'y':
+            team = None
+    session.add(model.SourceTeamName(datasource='espn.com', name=t, teamid=id))
+session.commit()
+    
+# Display unknown NCAA Teams
 print("Unknown NCAA Teams: ", end="")
 unknown_ncaa_teams = find_unknown_teams(ncaagames)
 if len(unknown_ncaa_teams) > 0:
     print(", ".join(unknown_ncaa_teams))
 else:
     print("None.")
+# Loop through them and prompt for teamids
+for t in unknown_ncaa_teams:
+    team = None
+    while team is None:
+        id = input("Enter team id for " + t + ": ")
+        team = session.query(model.Team).filter(model.Team.id == id).one_or_none()
+        print(team)
+        if input('Correct? (y/n) ').lower() != 'y':
+            team = None
+    session.add(model.SourceTeamName(datasource='ncaa.org', name=t, teamid=id))
+session.commit()
     
+# Create new matches and look for unknown teams again
+create_matches(session)
+unknown_espn_teams = find_unknown_teams(espngames)
+unknown_ncaa_teams = find_unknown_teams(ncaagames)
 if len(unknown_espn_teams) + len(unknown_ncaa_teams) > 0:
     # To Do: handle this better. Perhaps prompts?
     raise Exception('Please add entries to "sourceteamname" table for the above team(s).')
+
+################################################################################
 
 # Check for ESPN or NCAA games without matches or with more than one match
 def get_mismatch(games):
@@ -254,6 +291,62 @@ for m in session.query(model.Match):
 if numdisagreements == 0:
     print("None.")
 
+################################################################################    
+
+# Create a game (model.Game and model.GameResult) from a matched pair
+def game_from_match(m):
+    # Sort out combining comments
+    comments = None
+    if (m.espngame.comments is not None) and (m.ncaagame.comments is not None):
+        comments = m.espngame.comments + ', ' + m.ncaagame.comments
+    elif m.espngame.comments is not None:
+        comments = m.espngame.comments
+    elif m.ncaagame.comments is not None:
+        comments = m.ncaagame.comments
+    # Create game, result
+    game = model.Game(date=m.espngame.date, seasonid=m.espngame.season.id,
+                      hometeamid=m.espngame.hometeamlink.teamid,
+                      awayteamid=m.espngame.awayteamlink.teamid,
+                      neutralsite=m.ncaagame.neutralsite, comments=comments)
+    if check_scores_same(m.espngame, m.ncaagame):
+        result = model.GameResult(homepoints = m.espngame.homepoints,
+                                  awaypoints = m.espngame.awaypoints,
+                                  overtimes = m.espngame.overtimes)
+    else:
+        print("Score needed for ", end="")
+        print_no_score(m.espngame)
+        homepoints = input('Home team points: ')
+        awaypoints = input('Away team points: ')
+        overtimes = input('Overtimes: ')
+        result = model.GameResult(homepoints = homepoints,
+                                  awaypoints = awaypoints,
+                                  overtimes = overtimes)
+    result.game = game
+    return game, result
+
+# Check if a model.Game already exists in the database before adding it to the session
+def game_is_duplicate(testgame, session):
+    potentialmatches = session.query(model.Game).filter(
+        sqlalchemy.or_(
+            sqlalchemy.and_(
+                model.Game.hometeamid == testgame.hometeamid, 
+                model.Game.awayteamid == testgame.awayteamid,
+                model.Game.date == testgame.date),
+            sqlalchemy.and_(
+                model.Game.hometeamid == testgame.awayteamid, 
+                model.Game.awayteamid == testgame.hometeamid,
+                model.Game.date == testgame.date,
+                testgame.neutralsite == True),
+        )
+    )
+    n = potentialmatches.count()
+    if n == 0:
+        return False
+    elif n == 1:
+        return True
+    else:
+        raise Exception('Duplicate games in database.')
+
 # Upload games if desired.
 print("\n")
 proceed = str(input("Upload all matched games? (y/n) ")).lower()
@@ -262,33 +355,124 @@ while proceed not in ['y','n']:
 
 if proceed == 'y':
     print("Uploading games...")
+    inserted = 0
+    duplicates = 0
     for m in session.query(model.Match):
-        if not check_scores_same(m.espngame, m.ncaagame):
-            # Skip score mismatches
-            continue
-        # Sort out combining comments
-        comments = None
-        if (m.espngame.comments is not None) and (m.ncaagame.comments is not None):
-            comments = m.espngame.comments + ', ' + m.ncaagame.comments
-        elif m.espngame.comments is not None:
-            comments = m.espngame.comments
-        elif m.ncaagame.comments is not None:
-            comments = m.ncaagame.comments
-        # Create game, result, add to session
-        game = model.Game(date=m.espngame.date, seasonid=m.espngame.season.id,
-                          hometeamid=m.espngame.hometeamlink.teamid,
-                          awayteamid=m.espngame.awayteamlink.teamid,
-                          neutralsite=m.ncaagame.neutralsite, comments=comments)
-        result = model.GameResult(homepoints = m.espngame.homepoints,
-                                  awaypoints = m.espngame.awaypoints,
-                                  overtimes = m.espngame.overtimes)
-        result.game = game
-        session.add(game)
-        session.add(result)
+        game, result = game_from_match(m)
+        if not game_is_duplicate(game, session):
+            inserted += 1
+            session.add(game)
+            session.add(result)
+        else:
+            duplicates += 1
         # Delete the temporary rows we used to make this game
         session.delete(m.espngame)
         session.delete(m.ncaagame)
         session.delete(m)
     session.commit()
+    print('Newly inserted:', inserted)
+    print('Duplicates not inserted:', duplicates)
 
+################################################################################
 
+# Create a game from an unmatched ESPN game, prompting for info as necessary
+def game_from_espn(espn, neutralsite=None, comment='Missing from NCAA.org'):
+    # Create full comment
+    if espn.comments is not None:
+        comment = espn.comments + ', ' + comment
+    # Prompt user for neutralsite, if necessary
+    while neutralsite is None:
+        neutralsite = input('Neutral Site: (y/n) ').lower()
+        if neutralsite in ['y','n']:
+            neutralsite = (neutralsite == 'y')
+        else:
+            neutralsite = None
+    # Create game, result
+    game = model.Game(date=espn.date, seasonid=espn.season.id,
+                      hometeamid=espn.hometeamlink.teamid,
+                      awayteamid=espn.awayteamlink.teamid,
+                      neutralsite=neutralsite, comments=comment)
+    result = model.GameResult(homepoints = espn.homepoints,
+                              awaypoints = espn.awaypoints,
+                              overtimes = espn.overtimes)
+    result.game = game
+    return game, result
+
+# Create a game from an unmatched NCAA game, prompting for info as necessary
+def game_from_ncaa(ncaa, overtimes=None, comment='Missing from ESPN.com'):
+    # Create full comment
+    if ncaa.comments is not None:
+        comment = ncaa.comments + ', ' + comment
+    # Prompt user for overtimes, if necessary
+    while overtimes is None:
+        overtimes = input('Overtimes: ')
+        try:
+            overtimes = int(overtimes)
+        except:
+            overtimes = None
+    # Create game, result
+    game = model.Game(date=ncaa.date, seasonid=ncaa.season.id,
+                      hometeamid=ncaa.hometeamlink.teamid,
+                      awayteamid=ncaa.awayteamlink.teamid,
+                      neutralsite=ncaa.neutralsite, comments=comment)
+    result = model.GameResult(homepoints = ncaa.homepoints,
+                              awaypoints = ncaa.awaypoints,
+                              overtimes = overtimes)
+    result.game = game
+    return game, result
+
+# Upload ESPN games if desired, prompting for each.
+print("\n")
+proceed = str(input("Upload unmatched ESPN games? (y/n) ")).lower()
+while proceed not in ['y','n']:
+    proceed = str(input("Upload unmatched ESPN games? (y/n) ")).lower()
+
+if proceed == 'y':
+    inserted = 0
+    duplicates = 0
+    for espn in espngames:
+        print_with_score(espn)
+        creategame = 'x'
+        while creategame not in ['y','n']:
+            creategame = str(input("Create this game? (y/n) ")).lower()
+        if creategame == 'y':
+            game, result = game_from_espn(espn)
+            if not game_is_duplicate(game, session):
+                inserted += 1
+                session.add(game)
+                session.add(result)
+            else:
+                duplicates += 1
+            # Delete the temporary rows we used to make this game
+            session.delete(espn)
+    session.commit()
+    print('Newly inserted:', inserted)
+    print('Duplicates not inserted:', duplicates)
+    
+# Upload NCAA games if desired, prompting for each.
+print("\n")
+proceed = str(input("Upload unmatched NCAA games? (y/n) ")).lower()
+while proceed not in ['y','n']:
+    proceed = str(input("Upload unmatched NCAA games? (y/n) ")).lower()
+
+if proceed == 'y':
+    inserted = 0
+    duplicates = 0
+    for ncaa in ncaagames:
+        print_with_score(ncaa)
+        creategame = 'x'
+        while creategame not in ['y','n']:
+            creategame = str(input("Create this game? (y/n) ")).lower()
+        if creategame == 'y':
+            game, result = game_from_ncaa(ncaa)
+            if not game_is_duplicate(game, session):
+                inserted += 1
+                session.add(game)
+                session.add(result)
+            else:
+                duplicates += 1
+            # Delete the temporary rows we used to make this game
+            session.delete(ncaa)
+    session.commit()
+    print('Newly inserted:', inserted)
+    print('Duplicates not inserted:', duplicates)
